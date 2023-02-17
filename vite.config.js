03df38e88621c212a7be22a85a0ae382bdb97961
zwiro <josiu100@gmail.com>
2023-02-17 18:47:34 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://zwiro.github.io/portfolio/",
+  base: "/portfolio/",
   plugins: [react()],
 })
